527faea1d144c79c3e99b91de08fa9af9e1cf631
Keegan Street <kestreet@deloitte.com.au>
2016-08-22 21:26:39 +1000

    Added background-position-x-y, calc and canvas tests

---
+++ spec/fixtures/canvas.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById('tutorial');
+
+if (canvas.getContext) {
+	var ctx = canvas.getContext('2d');
+	// drawing code here
+} else {
+	// canvas-unsupported code here
+}
